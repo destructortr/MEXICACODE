1fb6c0b74605baade9283c9edde5fe1e6bff49ec
LuisF <LuisF@DESKTOP-ND387K3>
2019-03-20 11:17:32 -0600

    Siguientes cambios. Avance en la comunicación de vistas distintas.

--- QualytHome/src/main/java/com/masterdev/student/middle/TextFieldMethods.java
+++ QualytHome/src/main/java/com/masterdev/student/middle/TextFieldMethods.java
@@ -1,33 +1,11 @@
 package com.masterdev.student.middle;
 
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
-
-import java.io.IOException;
 
 import org.controlsfx.control.textfield.TextFields;
-
-import com.masterdev.student.middle.MiddleDemo;
-import com.masterdev.student.middle.Authentication;
-import com.masterdev.student.middle.Languages;
-import com.masterdev.student.middle.DashboardButtonAnimations;
-import com.masterdev.student.views.Dashboard;
-import com.masterdev.student.views.Login;
 
 public class TextFieldMethods {
 
--- QualytHome/src/main/java/com/masterdev/student/views/controllers/WarehouseController.java
+++ QualytHome/src/main/java/com/masterdev/student/views/controllers/WarehouseController.java
@@ -1,10 +1,14 @@
 package com.masterdev.student.views.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -17,6 +21,7 @@ import com.masterdev.student.middle.TextFieldMethods;
 import com.masterdev.student.middle.ComboBoxMethods;
 import com.masterdev.student.middle.DashboardButtonAnimations;
 import com.masterdev.student.middle.ImageViewMethods;
+import com.masterdev.student.middle.TableViewMethods;
 import com.masterdev.student.views.Home;
 import com.masterdev.student.views.Personal;
 import com.masterdev.student.views.Warehouse;
@@ -24,9 +29,26 @@ import com.masterdev.student.views.WarehouseForm;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 
-public class WarehouseController {
+public class WarehouseController implements Initializable {
 	@FXML Button btnAddWarehouse;
 	@FXML Button btnAddAsset;
+	
+	@FXML TableView<String> tabWarehouse;
+	
+	
+	public void initialize(URL location, ResourceBundle resources) {
+		//TableViewMethods tvm = new TableViewMethods();
+		//tvm.setWarehouseTable(tabWarehouse);
+		ObservableList<String> data = FXCollections.observableArrayList(
+		            new String("Hello"),
+		            new String("Goodbye"),
+		            new String("What's up?"),
+		            new String("How are you?"),
+		            new String("How's everything?")
+		); 
+		
+
+	}
 	
 	@FXML 
 	protected void openWarehouseForm() {
@@ -38,4 +60,5 @@ public class WarehouseController {
 	protected void openAssetForm() {
 		
 	}
+
 }
